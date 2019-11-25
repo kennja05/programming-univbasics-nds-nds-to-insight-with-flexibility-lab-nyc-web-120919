273c81a4ee94c13e59115f980b2832f391d470a0
Jacob Kenny <jacobkenny05@gmail.com>
2019-11-25 18:13:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -112,7 +112,7 @@ while director_index < source.length
      end 
      movie_index += 1 
    end 
-  final_array << [{:director_name => source[director_index][:name], :movies => source}]
+  final_array << [{:director_name => source[director_index][:name], :movies => source[director_index][:movies]}]
   #binding.pry
   director_index += 1 
   #binding.pry
