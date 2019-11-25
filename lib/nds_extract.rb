da3cbb7db8f96def97cac6cef6af7a29ee7f8ac8
Jacob Kenny <jacobkenny05@gmail.com>
2019-11-25 18:32:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -105,8 +105,7 @@ def movies_with_directors_set(source)
 director_index = 0
 final_array = []
 while director_index < source.length
-  final_array << [{:director_name => source[director_index][:name], :movies => source[director_index][:movies]}]
-  #binding.pry
+  final_array << movies_with_director_key(source[director_index][:name], source[director_index][:movies])
   director_index += 1 
   #binding.pry
  end
